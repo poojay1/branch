10f95d052dda88e0a9db349cfb70d13cc3eab014
prachiaga <prachiaga1997@gmail.com>
2017-06-08 12:33:40 +0530

    Create tree.R

--- tree.R
+++ tree.R
@@ -1,1 +1,13 @@
+options(java.parameters = "-Xmx4g" )
+require(XLConnect)
+readline(prompt="Choose a file:(Press Enter)")
+file=loadWorkbook(file.choose(),create=FALSE)
+
+f1=readWorksheet(file,sheet=1)
+level_no=rep(0,nrow(f1))
+f1=f1[1:nrow(f1),]
+master=data.frame(f1,level_no)
+
+
+	
 
